513dcdcfae93b12b36b7bc04bda9528836d24425
naiello <naiello@nd.edu>
2016-11-02 01:53:39 -0400

    more section page formatting

--- public/scripts/section.js
+++ public/scripts/section.js
@@ -16,14 +16,41 @@ $(function() {
             return;
         }
 
+        classYears = [
+            "Unknown",
+            "Freshman",
+            "Sophomore",
+            "Junior",
+            "Senior",
+            "5th Year / Grad"
+        ];
+
+        statusColors = {
+            Injured: 'text-bad',
+            Suspended: 'text-bad',
+            Absent: 'text-warn',
+            Inactive: 'text-warn'
+        }
+
         $table = $('table tbody');
+        var level = "Returning";
+        var status;
         for (var i = 0; i < mem.length; i++) {
+            if (mem[i].core == "Y") {
+                level = "CORE";
+            } else if (mem[i].new_member) {
+                level = "New Member";
+            }
+            status = mem[i].status;
+            if (status && statusColors.hasOwnProperty(status)) {
+                status = '<span class="' + statusColors[status] + '">' + status + "</span>";
+            }
             $table.append('<tr>' +
                 '<td>' + mem[i].first_name + '</td>' +
                 '<td>' + mem[i].last_name + '</td>' +
-                '<td>' + mem[i].class + '</td>' +
-                '<td>' + mem[i].level + '</td>' +
-                '<td>' + mem[i].status + '</td>' +
+                '<td>' + classYears[mem[i].year] + '</td>' +
+                '<td>' + level + '</td>' +
+                '<td>' + tatus + '</td>' +
                 '<td><a href="#">Edit</a></td><tr>'
             );
         }
